8b45f7e1232c886dc1a2be2e4363c97d1c889442
hoodsky <Hoodsky.ks@gmail.com>
2022-05-24 11:48:18 +0300

    fds

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const navMenu = document.querySelector('.nav-menu');
 navMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+const checkBox = document.querySelector('.checkbox');
+const checkBoxLabel = document.querySelector('.checkbox-label');
+
+checkBox.addEventListener('click', () => {
+    checkBoxLabel.classList.toggle('active');
+})
