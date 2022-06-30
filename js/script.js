989635684b9425671d246e7e03463f836fd35e8a
hoodsky <Hoodsky.ks@gmail.com>
2022-06-30 21:24:45 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ var checkBoxLabel = document.querySelector('.checkbox-label');
 checkBox.addEventListener('click', () => {
     checkBoxLabel.classList.toggle('active');
 })
+
+function average(array) {
+    let sum = 0;
+    let i = 0;
+    while (i < array.length) {
+        sum = sum + array[i];
+        i++;
+    }
+    console.log(sum / array.length)
+}
+
+average([2, 2, 5, 8]);
+
+function log() {
+    console.log("Delete");
+    for (let i = 0; i < 5; i++) {
+        console.log("create");
+    }
+}
+
+log();
